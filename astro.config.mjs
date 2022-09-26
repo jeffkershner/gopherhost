53176a1ac901fc1ce12499104ad625b656d7ff62
Jeff Kershner <jeff@jeffkershner.com>
2022-09-26 12:46:51 -0700

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,15 @@ import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import partytown from "@astrojs/partytown";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    svelte(),
-    partytown({
-      // Adds dataLayer.push as a forwarding-event.
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-  ],
+  site: 'https://gopherhost.com',
+  integrations: [svelte(), partytown({
+    // Adds dataLayer.push as a forwarding-event.
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  }), sitemap()]
 });
